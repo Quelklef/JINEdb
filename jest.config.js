f1c33a4c5f622477866d546be6325562c7701da1
Eli <elimaynard923@gmail.com>
2020-05-22 18:08:41 -0700

    install jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  reporters: [
+    'default',
+    ['./node_modules/jest-html-reporter/', {
+      'includeConsoleLog': true,
+      'includeFailureMsg': true,
+      'sort': 'status'
+    }],
+  ],
 };
