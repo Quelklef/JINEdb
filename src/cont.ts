6ec965820ce3bab4277f7c4fd36c965fc7fde4dc
Eli <elimaynard923@gmail.com>
2020-06-21 13:14:27 -0700

    remove Cont#trivial and AsyncCont#trivial They were never idiomatic, and now their time has come

--- src/cont.ts
+++ src/cont.ts
@@ -9,26 +9,10 @@ export class Cont<T>  {
   /** The wrapped value, expressed as a double-negation under Curry-Howard */
   private readonly nn_val: <R>(callback: (value: T) => R) => R;
 
-  /**
-   * True iff this was made via [[Cont.fromValue]] and then possibly mapped
-   * over, etc.
-   *
-   * If a continuation is trivial, we know that (1) its value will not change
-   * between calls to [[Cont.run]] and (2) the continuation has no "before"
-   * or "after" operations. Thus, the continuation may be treated as the value
-   * it wraps.
-   *
-   * Note that a continuation may satisfy both of these but be made with something
-   * other than [[Cont.fromValue]] and thus will not register as trivial.
-   */
-  public readonly trivial: boolean
-
   private constructor(
     nn_val: <R>(callback: (value: T) => R) => R,
-    trivial: boolean,
   ) {
     this.nn_val = nn_val;
-    this.trivial = trivial;
   }
 
   run<R>(f: (value: T) => R): R {
@@ -47,15 +31,15 @@ export class Cont<T>  {
 
   /** Monad.return */
   static fromValue<T>(val: T): Cont<T> {
-    return new Cont(k => k(val), true);
+    return new Cont(k => k(val));
   }
 
   static fromProducer<T>(prod: () => T): Cont<T> {
-    return new Cont(k => k(prod()), false);
+    return new Cont(k => k(prod()));
   }
 
   static fromFunc<T>(func: <R>(callback: (value: T) => R) => R): Cont<T> {
-    return new Cont(func, false);
+    return new Cont(func);
   }
 
   /** Monad.`>>=` */
@@ -63,16 +47,12 @@ export class Cont<T>  {
   //   return this.nn_val(x => f(x))
   // but this would fix the value of x, which is not desirable
   bind<S>(f: (value: T) => Cont<S>): Cont<S> {
-    if (this.trivial) {
-      return f(this.unwrap());
-    } else {
-      return new Cont(k => this.nn_val(x => f(x).nn_val(y => k(y))), false);
-    }
+    return new Cont(k => this.nn_val(x => f(x).nn_val(y => k(y))));
   }
 
   /** Functor.fmap */
   map<S>(f: (value: T) => S): Cont<S> {
-    return new Cont(k => this.nn_val(x => k(f(x))), this.trivial);
+    return new Cont(k => this.nn_val(x => k(f(x))));
   }
 
 }
@@ -86,14 +66,11 @@ export class Cont<T>  {
 export class AsyncCont<T> {
   
     private readonly nn_val: <R>(callback: (value: Awaitable<T>) => R) => R;
-    public readonly trivial: boolean;
 
     private constructor(
         nn_val: <R>(callback: (value: Awaitable<T>) => R) => R,
-        trivial: boolean,
     ) {
       this.nn_val = nn_val;
-      this.trivial = trivial;
     }
     
     run<R>(f: (value: T) => Awaitable<R>): Awaitable<R> {
@@ -108,19 +85,19 @@ export class AsyncCont<T> {
     }
 
     static fromValue<T>(x: Awaitable<T>): AsyncCont<T> {
-      return new AsyncCont(k => k(x), true);
+      return new AsyncCont(k => k(x));
     }
 
     static fromProducer<T>(prod: () => Awaitable<T>): AsyncCont<T> {
-      return new AsyncCont(k => k(prod()), false);
+      return new AsyncCont(k => k(prod()));
     }
 
     static fromFunc<T>(func: <R>(callback: (value: Promise<T>) => R) => R): AsyncCont<T> {
-      return new AsyncCont(func, false);
+      return new AsyncCont(func);
     }
 
     map<S>(f: (value: T) => Awaitable<S>): AsyncCont<S> {
-      return new AsyncCont(k => this.nn_val(x => x instanceof Promise ? k(x.then(f)) : k(f(x))), this.trivial);
+      return new AsyncCont(k => this.nn_val(x => x instanceof Promise ? k(x.then(f)) : k(f(x))));
     }
 
 }
